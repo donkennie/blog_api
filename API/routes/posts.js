336907f30fbd9108a08207017bf0d4fb99882454
donkennie <ajeigbekehinde160@gmail.com>
2022-07-04 14:39:19 +0100

    implement on CRUD endpoints

--- API/routes/posts.js
+++ API/routes/posts.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
 
   try {
-    const savedPost = newPost.save();
+    const savedPost = await newPost.save();
     res.status(200).json(savedPost);
   } catch (err) {
     res.status(500).json(err);
@@ -18,27 +18,53 @@ router.post("/", async (req, res) => {
 
 //UPDATE POST
 
-router.delete("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
+    if (post.username === req.body.username) {
       try {
-        await Post.deleteMany({ username: user.username });
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted...");
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+
+          { new: true }
+        );
+
+        res.status(200).json(updatedPost);
       } catch (err) {
         res.status(500).json(err);
       }
-    } catch (err) {
-      res.status(404).json("User not found!");
+    } else {
+      res.status(401).json("You can update only your post!");
     }
-  } else {
-    res.status(401).json("You can delete only your account!");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 //DELETE POST
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //GET POST
 
