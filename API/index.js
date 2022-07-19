c94332ae86cc7e252c1c26b768cfb873a165079a
donkennie <ajeigbekehinde160@gmail.com>
2022-07-19 16:25:03 +0100

    add upload image endpoint

--- API/index.js
+++ API/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+
 const app = express();
 
 const dotenv = require("dotenv");
+
 const mongoose = require("mongoose");
 
 const authRoute = require("./routes/auth");
@@ -11,6 +13,8 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 
 const categoryRoute = require("./routes/categories");
+
+const multer = require("multer");
 
 dotenv.config();
 
@@ -23,6 +27,21 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
--- API/routes/categories.js
+++ API/routes/categories.js
@@ -6,8 +6,18 @@ router.post("/", async (req, res) => {
   const newCat = new Category(req.body);
 
   try {
-    const savedCat = await new newCat.save();
+    const savedCat = await newCat.save();
     res.status(200).json(savedCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const cats = await new Category.find();
+
+    res.status(200).json(cats);
   } catch (err) {
     res.status(500).json(err);
   }
